f665d4f1bf4dff8748601d355da561e23e9ff0a3
Mariya5025 <139927721+Mariya5025@users.noreply.github.com>
2023-07-21 15:56:26 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,12 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+   let discr = b**2-4*a*c;
+  //return arr;
+   if (discr < 0) {
+    arr = [];
+  } else if (discr === 0){
+    let root = -b/(2*a);
+     arr.push(root);     
+  } else if (discr > 0) {
+    let oneroot = (-b + Math.sqrt(discr))/(2*a);
+    let tworoot = (-b - Math.sqrt(discr))/(2*a);
+     arr.push(oneroot);
+     arr.push(tworoot);     
+  }
+  return (arr);
   
-  return arr;
-}
-
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
-git add %file-path%
-git push -u origin main
