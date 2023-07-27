46a67843155bfcd35d58bc963894e1f8da5e79ad
Mariya5025 <139927721+Mariya5025@users.noreply.github.com>
2023-07-27 06:30:24 +0300

    Update task.js

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -34,17 +34,4 @@ class Triangle {
     }
 }
 
-function getTriangle(a, b, c) {
-    try {
-        return new Triangle(a, b, c);
-    } catch(error) {
-        return {
-            get area() {
-                return "Ошибка! Треугольник не существует";
-            },
-            get perimeter() {
-                return "Ошибка! Треугольник не существует";
-            }
-        };
-    }
-}
+ 
