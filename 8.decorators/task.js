b2cfb91d1d69ee8ef312fc7bc70bc011e34296ab
Mariya5025 <139927721+Mariya5025@users.noreply.github.com>
2023-07-29 00:11:04 +0300

    Update task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,49 +1,49 @@
  function cachingDecoratorNew(func) {
-  let cache = [];
+    let cache = [];
+    return function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash === hash);
+        if(objectInCache) { 
+            console.log("Из кэша: " + objectInCache.result)
+            return "Из кэша: " + objectInCache.result;
+        }
 
-function wrapper(...args) {
-    const hash = args.join(','); // получаем правильный хэш
-    let idx = cache.findIndex((item)=> item.hash === hash ); // ищем элемент, хэш которого равен нашему хэшу
-    if (idx !== -1 ) { // если элемент не найден
-        console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-        return "Из кэша: " + cache[idx].result;
+        let result = func(...args);
+        cache.push({hash, result});
+        if (cache.length > 5) {
+            cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
     }
-
-    let result = func(...args); // в кэше результата нет - придётся считать
-    cache.push({hash, result}) ; // добавляем элемент с правильной структурой
-    if (cache.length > 5) { 
-      cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
-    }
-    console.log("Вычисляем: " + result);
-    return "Вычисляем: " + result;  
-}
-return wrapper;
 }
 
+ 
+//Задача № 2
 function debounceDecoratorNew(func, delay) {
-  let timeoutId;
-  let hasCalled = false;
-  function doIt(args) { // Убираем дубликат кода
-    func.apply(this, args);
-    hasCalled = true;
-    wrapper.count++; // Увеличиваем сигналы
-  }
+    let timeoutId = null;
+    function wrapper(...args) {
+        if(timeoutId === null) {
+            func(...args);
+            wrapper.count++;
+        }
 
-  function wrapper(...args) {
-    if (timeoutId) clearTimeout(timeoutId);
-    if (!wrapper.count) wrapper.count = 0;
-    if (!wrapper.allCount) wrapper.allCount = 0;
+        if(timeoutId) {  //Повторил код лекции 
+            clearTimeout(timeoutId);
+        }
 
-    if (!hasCalled) {
-      doIt(args);
-    } else {
-      wrapper.allCount++; // Увеличиваем вызовы
-      timeoutId = setTimeout(() => doIt(args), delay);
+        timeoutId = setTimeout(() => {
+            timeoutId = true;
+            func(...args);
+            wrapper.count++;
+        }, delay);
+        wrapper.allCount++
     }
-  }
-  return wrapper;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
 }
-
+    
  
  
 
